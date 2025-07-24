f3f936bf9f6366e183a8b75c4a0b96f0eba268f0
Orjihenry <orjihen@yahoo.com>
2025-07-24 20:44:51 -0230

    added Footer component to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import NavButton from "../../components/NavButton";
 import "./Home.css";
@@ -27,6 +28,7 @@ export default function Home() {
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 }
