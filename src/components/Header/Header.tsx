79af4c8a371b548d6fbac154963bed8b6a1eff84
Orjihenry <orjihen@yahoo.com>
2025-07-24 15:04:15 -0230

    refactor Header component to use NavButton for anchr links and Button component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,62 @@
 // import 'Header.css'
+import NavButton from "../NavButton";
+import Button from "../Button";
 
 export default function Header() {
-    return (
-        <header className="header">
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container ">
-                    <a className="navbar-brand" href="#">Read2Geda</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+  return (
+    <header className="header">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container ">
+          <a className="navbar-brand" href="#">
+            Read2Geda
+          </a>
+          <Button
+            className="navbar-toggler"
+            type="button"
+            dataBsToggle="collapse"
+            dataBsTarget="#navbarSupportedContent"
+            ariaExpanded={false}
+            ariaControls="navbarSupportedContent"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </Button>
 
-                    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link active" href="#">Discover</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">My Shelf</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Highlights</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">How it works</a>
-                            </li>
-                        </ul>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link active" href="#">
+                  Discover
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  My Shelf
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Highlights
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  How it works
+                </a>
+              </li>
+            </ul>
 
-                        <button className="btn btn-outline-success m-md-2 my-sm-0" type="submit">Login / Register</button>
-
-                    </div>
-                    
-                </div>
-            </nav>
-        </header>
-    )
+            <NavButton
+              href="/login"
+              className=" m-md-2 my-sm-0"
+              label="Login / Register"
+            />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
