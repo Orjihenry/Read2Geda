4a05871935b8ddad05c115516503f853a647655c
Orjihenry <orjihen@yahoo.com>
2025-07-25 01:56:36 -0230

    added Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,25 @@
+import './Card.css'
+
+type CardProps = {
+  children?: React.ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
+  imgSrc?: string;
+  imgAlt: string;
+  title?: string;
+  text?: string;
+}
+
+export default function Card({ children, className = "", style = {}, imgSrc, imgAlt = "", title, text,
+}: CardProps) {
+  return (
+    <div className={`card ${className}`} style={style}>
+      {imgSrc && <img className="card-img-top" src={imgSrc} alt={imgAlt} />}
+      <div className="card-body">
+        {title && <h5 className="card-title">{title}</h5>}
+        {text && <p className="card-text">{text}</p>}
+        {children}
+      </div>
+    </div>
+  );
+}
