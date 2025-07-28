68618ef16082fc815f135ddbbb740dacba6450db
Orjihenry <orjihen@yahoo.com>
2025-07-28 00:22:40 -0230

    added icons and removed unused styles

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,6 @@
 import { defaultBookClubs } from "../../utils/bookClub";
+import { MdAddLocationAlt, MdCalendarMonth, MdPeopleAlt, MdStar } from "react-icons/md";
+import { IoMdPricetag } from "react-icons/io";
 import { useEffect } from "react";
 import Slider from "react-slick";
 import NavButton from "../NavButton";
@@ -59,7 +61,7 @@ export default function Carousel() {
         <div className="slider-container">
             <Slider {...settings}>
             {defaultBookClubs.map((item, index) => (
-            <div key={index} className="px-2">
+            <div key={index} className="px-2 py-5">
                 <div className="p-3 border rounded shadow-sm bg-light card-custom-wrapper">
                     <div className="d-flex text-start align-items-center">
                         <div className="flex-shrink-0 me-3">
@@ -77,21 +79,19 @@ export default function Carousel() {
 
                         </div>
                     </div>
-                    <div className="d-flex mt-2 justify-content-center gap-2">
-                        <p className="mb-0 small px-2 border-right">Location: {item.location || "Online"}</p>
-                        <p className="mb-0 small"> Members: {item.members.length}</p>
-                    </div>
-                    <div className="">
-                        <div className="d-flex align-items-center justify-content-center">
-                            <span className="me-2">⭐ Rating:</span>
+                    <div className="mt-2">
+                        <p className="mb-0 small d-flex align-items-center justify-content-start"><span className="pe-2"><MdAddLocationAlt /></span> Location: {item.location || "Online"} But offline</p>
+                        <p className="mb-0 small d-flex align-items-center justify-content-start"><span className="pe-2"><MdPeopleAlt /></span> Members: {item.members.length}</p>
+                        <div className="d-flex align-items-center justify-content-start">
+                            <span className="me-2"><span className="pe-2"><MdStar /></span>Rating:</span>
                             <div className="d-flex">
                                 {Array.from({ length: 5 }, (_, i) => (
                                     <span key={i} className={`star ${i < item.rating ? "filled" : ""}`}>★</span>
                                 ))}
                             </div>
                         </div>
-                        <p className="mb-1 small text-muted"><span className="font-weight-bold">🏷️ Tags:</span> {item.tags?.join(", ") || "None"}</p>
-                        <p className="mb-1 small">📅 Next Meeting: {item.nextMeeting ? new Date(item.nextMeeting).toLocaleDateString() : "N/A"}</p>
+                        <p className="mb-1 small d-flex align-items-center justify-content-start"><span className="font-weight-bold"><span className="pe-2"><IoMdPricetag /></span>Tags: </span> {item.tags?.join(", ") || "None"}</p>
+                        <p className="mb-1 small d-flex align-items-center justify-content-start"><span className="pe-2"><MdCalendarMonth /></span>Next Meeting: {item.nextMeeting ? new Date(item.nextMeeting).toLocaleDateString() : "N/A"}</p>
                     </div>
                     <div className="d-flex gap-2 pt-2 justify-content-center">
                         <NavButton className="btn-dark text-light btn-sm" href={`#/book-club/${item.id}`} label="Join Club" />
