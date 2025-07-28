b1f709f23e0a3a121b94ebcfd9547c3b2fc98fcc
Orjihenry <orjihen@yahoo.com>
2025-07-28 02:03:19 -0230

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Header from './components/Header'
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
 
   return (
     <>
-      <Header />
+      <Router>
+        <Routes>
+          <Route path="/Read2Geda" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Read2Geda/',
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   plugins: [react()],
 })
