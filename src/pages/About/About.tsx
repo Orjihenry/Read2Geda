5aaadd7e7fde4f10cc594ba66e706a81a5cd72f1
Orjihenry <orjihen@yahoo.com>
2025-07-28 23:38:56 -0230

    added About page and related files

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,13 @@
+
+
+export default function About() {
+    return (
+        <>
+            <div className="container text-center my-5">
+                <h1>
+                    About Read2Geda
+                </h1>
+            </div>
+        </>
+    )
+}
