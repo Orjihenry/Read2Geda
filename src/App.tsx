3662dd9fcac731b9ffe018b82ecc2dfa3faad8d4
Orjihenry <orjihen@yahoo.com>
2025-07-28 23:40:27 -0230

    refactored import paths for Home and added About component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import Home from './pages/Home';
+import About from './pages/About';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle';
 
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
         </Routes>
       </Router>
     </>
