9c9f875d029308340a49b61d0c3bacc87a5245e6
Orjihenry <orjihen@yahoo.com>
2025-07-29 09:15:20 -0230

    refactored Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,16 +10,17 @@ type CardProps = {
   text?: string;
 }
 
-export default function Card({ children, className = "", style = {}, imgSrc, imgAlt = "", title, text,
+export default function Card(
+    { children, className = "", style = {}, imgSrc, imgAlt = "", title, text,
 }: CardProps) {
   return (
     <div className={`card mb-5 ${className}`} style={style}>
-      {imgSrc && <img className="card-img-top" src={imgSrc} alt={imgAlt} />}
-      <div className="card-body">
-        {title && <h5 className="card-title">{title}</h5>}
-        {text && <p className="card-text">{text}</p>}
-        {children}
-      </div>
+    {imgSrc && <img className="card-img-top" src={imgSrc} alt={imgAlt} />}
+        <div className="card-body">
+            {title && <h5 className="card-title">{title}</h5>}
+            {text && <p className="card-text">{text}</p>}
+            {children}
+        </div>
     </div>
   );
 }
