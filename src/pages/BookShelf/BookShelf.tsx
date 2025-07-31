b4d089b131e361205acb17a8f38dc63d15ee414e
Orjihenry <orjihen@yahoo.com>
2025-07-30 22:13:19 -0230

    added pages

---
+++ src/pages/BookShelf/BookShelf.tsx
@@ -0,0 +1,11 @@
+
+
+export default function BookShelf() {
+    return (
+        <div className="container">
+            <h1 className="text-center">
+                MyShelf
+            </h1>
+        </div>
+    )
+}
