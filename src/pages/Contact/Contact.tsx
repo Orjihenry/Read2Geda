0683f241a9c38d977bc2d9bd15ff6a10c4654588
Orjihenry <orjihen@yahoo.com>
2025-07-30 22:14:25 -0230

    added contact page

---
+++ src/pages/Contact/Contact.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Contact() {
+    return (
+        <div className="container">
+            <h1 className="text-center">
+                Contact Us
+            </h1>
+        </div>
+    )
+}
