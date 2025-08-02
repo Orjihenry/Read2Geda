99a38ce9eb5781de8be6f10f6cafd8a73ccb4035
Orjihenry <orjihen@yahoo.com>
2025-08-01 23:21:54 -0230

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/read2geda/",
+  base: "/Read2Geda/",
   resolve: {
     alias: {
       '@': '/src',
