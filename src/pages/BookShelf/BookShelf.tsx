da9b43e2e3b43b75bf1552d07db77752fa1db824
Orjihenry <orjihen@yahoo.com>
2025-08-03 22:46:29 -0230

    added book shelf data

--- src/pages/BookShelf/BookShelf.tsx
+++ src/pages/BookShelf/BookShelf.tsx
@@ -1,11 +1,37 @@
-
+import BookCard from "../../components/BookCard";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import useBookData from "../../hooks/useBookData";
 
 export default function BookShelf() {
-    return (
-        <div className="container">
-            <h1 className="text-center">
-                MyShelf
-            </h1>
+    const { books, loading } = useBookData();
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+
+        <div className="row g-3 my-5">
+          <h1 className="display-6 py-3">Book Shelf</h1>
+
+          {loading ? (
+            <div className="text-center py-5">
+              <div className="spinner-border text-primary mb-2" role="status" />
+              <p>📚 Please wait while we load your next great read...</p>
+            </div>
+          ) : books && books.length > 0 ? (
+            books.map((item, index) => (
+              <div key={item.id || index} className="col-md-4">
+                <BookCard item={item} index={index} />
+              </div>
+            ))
+          ) : (
+            <p className="text-muted">No book clubs available right now.</p>
+          )}
         </div>
-    )
+      </div>
+
+      <Footer />
+    </>
+  );
 }
