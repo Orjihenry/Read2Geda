7e9089b6f21427f223a7ae1661260b05712040db
Orjihenry <orjihen@yahoo.com>
2025-08-16 23:53:05 -0230

    added route - how it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
 import About from "./pages/About";
 import BookShelf from "./pages/BookShelf";
 import ClubDetails from "./pages/ClubDetails";
@@ -6,7 +8,10 @@ import Contact from "./pages/Contact";
 import CreateClub from "./pages/CreateClub";
 import Discover from "./pages/Discover";
 import Home from "./pages/Home";
+import HowItWoks from "./pages/HowItWorks";
 import Profile from "./pages/Profile";
+
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 
@@ -21,6 +26,7 @@ function App() {
           <Route path="/highlights" element={<Profile />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/my_shelf" element={<BookShelf />} />
+          <Route path="/how_it_works" element={<HowItWoks />} />
           <Route path="/club/:clubId" element={<ClubDetails />} />
           <Route path="/create_club" element={<CreateClub />} />
         </Routes>
