d1eaa07572e734f50d6e2de13452951a4b70e5cc
Orjihenry <orjihen@yahoo.com>
2025-08-19 23:57:16 -0230

    refactored hook to use API and fallback to local data is API fails

--- src/hooks/useBookData.ts
+++ src/hooks/useBookData.ts
@@ -3,14 +3,28 @@ import { bookData, type BookData } from "../utils/bookData";
 
 export default function useBookData() {
     const [books, setBooks] = useState<BookData[]>([]);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-        setTimeout(() => {
+        async function fetchBooks() {
+          setLoading(true);
+          try {
+            const response = await fetch("https://api.example.com/books");
+            if (!response.ok) throw new Error("API Failed, using local books data");
+      
+            const data: BookData[] = await response.json();
+            setBooks(data);
+          } catch (err) {
+            console.warn("Error fetching books from API:", err);
             setBooks(bookData);
+            setError("Using local books data");
+          } finally {
             setLoading(false);
-        }, 500);
-    }, []);
+          }
+        }
+        fetchBooks();
+      }, []);
 
-    return { books, loading };
+    return { books, loading, error };
 }
