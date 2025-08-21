000d3e9aa2ebdbed2029474125498cda125879d6
Orjihenry <orjihen@yahoo.com>
2025-08-20 23:56:46 -0230

    refactored useClubData hook to utilise localStorage

--- src/hooks/useClubData.ts
+++ src/hooks/useClubData.ts
@@ -2,15 +2,27 @@ import { useEffect, useState } from "react";
 import { defaultBookClubs, type bookClub } from "./../utils/bookClub";
 
 export default function useClubData() {
-    const [clubs, setClubs] = useState<bookClub[]>([]);
-    const [loading, setLoading] = useState(true);
+  const [clubs, setClubs] = useState<bookClub[]>([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setClubs(defaultBookClubs);
-            setLoading(false);
-        }, 500);
-    }, []);
+  useEffect(() => {
+    const savedClubs = localStorage.getItem("bookClubs");
 
-    return { clubs, loading };
+    if (savedClubs) {
+      setClubs(JSON.parse(savedClubs));
+    } else {
+      setClubs(defaultBookClubs);
+      localStorage.setItem("bookClubs", JSON.stringify(defaultBookClubs));
+    }
+
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (clubs.length > 0) {
+      localStorage.setItem("bookClubs", JSON.stringify(clubs));
+    }
+  }, [clubs]);
+
+  return { clubs, setClubs, loading };
 }
