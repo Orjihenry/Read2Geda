26377e73f6d97dbeb337a9858a225327f7b25fc6
Orjihenry <orjihen@yahoo.com>
2025-08-22 20:55:40 -0230

    added Login component and Auth page entry point

---
+++ src/pages/Auth/Login.tsx
@@ -0,0 +1,55 @@
+import { NavLink } from "react-router-dom";
+
+export default function Login() {
+    return (
+        <>
+          <section className="vh-100">
+            <div className="container-fluid h-100">
+              <div className="row h-100">
+                
+                <div className="col-md-6 d-flex justify-content-center align-items-center bg-light shadow">
+                  <div className="col-sm-10 col-md-8 col-lg-7">
+                    <div className="card shadow-sm border-0">
+                      <div className="card-header text-center bg-dark text-light border-0">
+                        <a href="/" className="navbar-brand p-3 fs-2 logo-glass d-flex justify-content-center align-items-center">
+                          <img className="rounded rounded-circle me-2" src="/src/assets/read2geda.ico" alt="logo" style={{ width: "40px "}} />
+                          Read2Geda
+                        </a>
+                      </div>
+                      <div className="card-body px-4 py-5 pt-3">
+                        <h3 className="fw-normal fs-6 mb-0 text-center">Log in</h3>
+                        <p className="text-center mb-4">Welcome back!</p>
+                        <form>
+                          <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email address</label>
+                            <input type="email" id="email" className="form-control" />
+                          </div>
+                          <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input type="password" id="password" className="form-control" />
+                          </div>
+                          <div className="form-check mb-3">
+                            <input type="checkbox" id="remember" className="form-check-input" />
+                            <label htmlFor="remember" className="form-check-label">Remember me</label>
+                          </div>
+                          <button type="submit" className="btn btn-outline-success w-100 mb-3">
+                            Sign in
+                          </button>
+                          <div className="text-center">
+                            <p>Not a member? <NavLink to="/register">Register</NavLink></p>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-6 d-none d-md-block p-0">
+                    <div className="h-100 w-100 bg-success"></div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </>
+    )
+}
