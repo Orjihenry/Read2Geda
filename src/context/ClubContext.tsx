096b8460ec441c5ba4ce29cda02c25d23a9ace02
Orjihenry <orjihen@yahoo.com>
2025-10-16 22:55:31 -0230

    Added joinClub and leaveClub functions

--- src/context/ClubContext.tsx
+++ src/context/ClubContext.tsx
@@ -1,84 +1,154 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { type bookClub, defaultBookClubs } from "../utils/bookClub";
 import { v4 as uuidv4 } from "uuid";
+import dayjs from "dayjs";
 
 type ClubContextType = {
-    clubs: bookClub[];
-    loading: boolean;
-    createClub: (club: bookClub) => void;
-    updateClub: (club: bookClub) => void;
-    deleteClub: (clubId: string) => void;
-    clubNameExists: (clubName: string) => boolean;
-}
+  clubs: bookClub[];
+  loading: boolean;
+  createClub: (club: bookClub) => void;
+  updateClub: (club: bookClub) => void;
+  deleteClub: (clubId: string) => void;
+  clubNameExists: (clubName: string) => boolean;
+  joinClub: (club: bookClub, userId: string) => void;
+  leaveClub: (clubId: string, userId: string) => void;
+};
 
 const ClubContext = createContext<ClubContextType | undefined>(undefined);
 
 export function ClubProvider({ children }: { children: React.ReactNode }) {
-    const [clubs, setClubs] = useState<bookClub[]>([]);
-    const [loading, setLoading] = useState(false);
+  const [clubs, setClubs] = useState<bookClub[]>([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const savedClubs = localStorage.getItem("bookClubs");
-        setLoading(true);
-        if (savedClubs) {
-            setClubs(JSON.parse(savedClubs));
-        } else {
-            setClubs(defaultBookClubs);
-            localStorage.setItem("bookClubs", JSON.stringify(defaultBookClubs));
-        }
-        setLoading(false);
-    }, [])
+  const today = dayjs();
+  const currentDate = today.format("YYYY-MM-DD HH:mm:ss");
 
-    const createClub = (club: bookClub) => {
-        const newClub = {
-            ...club,
-            id: uuidv4(),
-            createdAt: new Date().toISOString(),
-            updatedAt: new Date().toISOString(),
-            members: club.members,
-            ownerName: club.ownerName,
-            ownerImageUrl: club.ownerImageUrl,
-            isPublic: club.isPublic,
-            tags: club.tags,
-            location: club.location,
-            meetingFrequency: club.meetingFrequency,
-        }
-        setClubs([...clubs, newClub]);
-        localStorage.setItem("bookClubs", JSON.stringify([...clubs, newClub]));
+  useEffect(() => {
+    const savedClubs = localStorage.getItem("bookClubs");
+    setLoading(true);
+    if (savedClubs) {
+      setClubs(JSON.parse(savedClubs));
+    } else {
+      setClubs(defaultBookClubs);
+      localStorage.setItem("bookClubs", JSON.stringify(defaultBookClubs));
     }
+    setLoading(false);
+  }, []);
 
-    const updateClub = (club: bookClub) => {
-        const updateClub = {
-            ...club,
-            updatedAt: new Date().toISOString(),
-        }
-        setClubs(clubs.map(c => c.id === club.id ? updateClub : c));
-        localStorage.setItem("bookClubs", JSON.stringify(clubs.map(c => c.id === club.id ? updateClub : c)));
-    }
+  const createClub = (club: bookClub) => {
+    const newClub = {
+      ...club,
+      id: uuidv4(),
+      createdAt: currentDate,
+      updatedAt: currentDate,
+      members: club.members,
+      isPublic: club.isPublic,
+      tags: club.tags,
+      location: club.location,
+      meetingFrequency: club.meetingFrequency,
+    };
+    setClubs([...clubs, newClub]);
+    localStorage.setItem("bookClubs", JSON.stringify([...clubs, newClub]));
+  };
 
-    const deleteClub = (clubId: string) => {
-        setClubs(clubs.filter(c => c.id !== clubId));
-        localStorage.setItem("bookClubs", JSON.stringify(clubs.filter(c => c.id !== clubId)));
-    }
+  const updateClub = (club: bookClub) => {
+    const updateClub = {
+      ...club,
+      updatedAt: currentDate,
+    };
+    setClubs(clubs.map((c) => (c.id === club.id ? updateClub : c)));
+    localStorage.setItem(
+      "bookClubs",
+      JSON.stringify(clubs.map((c) => (c.id === club.id ? updateClub : c)))
+    );
+  };
 
-    const clubNameExists = (clubName: string) => {
-        return clubs.some(c => c.name === clubName);
-    }
+  const deleteClub = (clubId: string) => {
+    setClubs(clubs.filter((c) => c.id !== clubId));
+    localStorage.setItem(
+      "bookClubs",
+      JSON.stringify(clubs.filter((c) => c.id !== clubId))
+    );
+  };
 
-    return (
-        <ClubContext.Provider value={{ clubs, loading, createClub, updateClub, deleteClub, clubNameExists }}>
-            {children}
-        </ClubContext.Provider>
-    )
+  const clubNameExists = (clubName: string) => {
+    return clubs.some((c) => c.name === clubName);
+  };
+
+  const joinClub = (club: bookClub, userId: string) => {
+    if (!club) return;
+    if (club.members.some((m) => m.id === userId)) return;
+    const newMember = {
+      ...club,
+      members: [
+        ...club.members,
+        {
+          id: userId,
+          role: "member" as const,
+          joinedAt: currentDate,
+          isSuspended: false,
+        },
+      ],
+    };
+    const updatedClubs = clubs.map((c) => (c.id === club.id ? newMember : c));
+    setClubs(updatedClubs);
+    localStorage.setItem("bookClubs", JSON.stringify(updatedClubs));
+  };
+
+  const leaveClub = (clubId: string, userId: string) => {
+    const club = clubs.find((c) => c.id === clubId);
+    if (!club) return;
+
+    const updatedMembers = club.members.filter((m) => m.id !== userId);
+    const updatedClub = { ...club, members: updatedMembers };
+    const updatedClubs = clubs.map((c) => (c.id === clubId ? updatedClub : c));
+    setClubs(updatedClubs);
+    localStorage.setItem("bookClubs", JSON.stringify(updatedClubs));
+  };
+
+  return (
+    <ClubContext.Provider
+      value={{
+        clubs,
+        loading,
+        createClub,
+        updateClub,
+        deleteClub,
+        clubNameExists,
+        joinClub,
+        leaveClub,
+      }}
+    >
+      {children}
+    </ClubContext.Provider>
+  );
 }
 
 export function useClub() {
-    const context = useContext(ClubContext);
-    if (!context) throw new Error("useClub must be used within a ClubProvider");
-    return context;
+  const context = useContext(ClubContext);
+  if (!context) throw new Error("useClub must be used within a ClubProvider");
+  return context;
 }
 
 export function useClubData() {
-    const { clubs, loading, createClub, updateClub, deleteClub, clubNameExists } = useClub();
-    return { clubs, loading, createClub, updateClub, deleteClub, clubNameExists };
+  const {
+    clubs,
+    loading,
+    createClub,
+    updateClub,
+    deleteClub,
+    clubNameExists,
+    joinClub,
+    leaveClub,
+  } = useClub();
+  return {
+    clubs,
+    loading,
+    createClub,
+    updateClub,
+    deleteClub,
+    clubNameExists,
+    joinClub,
+    leaveClub,
+  };
 }
