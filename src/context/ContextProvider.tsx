8db496f3020e22e9fa4d921bc5d7fac46b6cae99
Orjihenry <orjihen@yahoo.com>
2025-11-15 23:24:54 -0330

    Integrated BookCacheProvider into ContextProvider for improved book data management

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -2,6 +2,7 @@ import { type ReactNode } from "react";
 import { SavedBooksProvider } from "./SavedBooksContext";
 import { ClubProvider } from "./ClubContext";
 import AuthProvider from "./AuthContext";
+import { BookCacheProvider } from "./BookCacheContext";
 
 interface ContextProviderProps {
   children: ReactNode;
@@ -10,13 +11,15 @@ interface ContextProviderProps {
 export function ContextProvider({ children }: ContextProviderProps) {
   return (
     <AuthProvider>
-      <SavedBooksProvider>
-        <ClubProvider>
-          {children}
-        </ClubProvider>
-      </SavedBooksProvider>
+      <BookCacheProvider>
+        <SavedBooksProvider>
+          <ClubProvider>
+            {children}
+          </ClubProvider>
+        </SavedBooksProvider>
+      </BookCacheProvider>
     </AuthProvider>
   );
 }
 
-export { SavedBooksProvider, ClubProvider, AuthProvider };
+export { SavedBooksProvider, ClubProvider, AuthProvider, BookCacheProvider };
